bc6ed49c4f41c204ec8e5907180c5469385382db
Ian D. McCunn <imccunn@expedia.com>
2016-08-23 16:27:05 -0700

    cleanup

--- server.js
+++ server.js
@@ -7,38 +7,38 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // app.get('/', function(req, res) {
-// 	res.sendFile(__dirname + '/app/index.html');
+//  res.sendFile(__dirname + '/app/index.html');
 // });
 
-app.use(express.static('app'));
+app.use(express.static('public'));
 
 var users = [];
 
 io.on('connection', function(socket) {
-	console.log('A user connected.');
+  console.log('A user connected.');
   // Track users by socket
   users.push(socket);
-	
-	socket.on('cMsg', function (nameMsg) {
-		var time = new Date();
-		var timeString = '' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+  
+  socket.on('cMsg', function (nameMsg) {
+    var time = new Date();
+    var timeString = '' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
 
-		console.log('message: ' + nameMsg[0] + ': ' + nameMsg[1]);
-		io.emit('cMsg', [timeString, nameMsg[0], nameMsg[1]]);
-	});
+    console.log('message: ' + nameMsg[0] + ': ' + nameMsg[1]);
+    io.emit('cMsg', [timeString, nameMsg[0], nameMsg[1]]);
+  });
   
   socket.on('typing', function(status) { // TODO: broadcast when user is typing
     io.emit('userTyping', status);
-  }); 	
+  });   
   socket.on('disconnect', function(socket) {
-		console.log('User disconnected.');
+    console.log('User disconnected.');
     var i = users.indexOf(socket);
     users.splice(i, 1);
-	});
+  });
 });
 
 http.listen(3000, function() {
-	console.log('Server listening on port 3000');
+  console.log('Server listening on port 3000');
 });
 
 // Broadcast a message to connected users when someone connects or disconnects
