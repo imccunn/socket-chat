53331d0f11d2a5932e2d39ea35d8f6bf2204d5f7
Ian McCunn <ianmccunn@gmail.com>
2016-08-27 11:20:18 -0700

    table layout

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function findWithAttr(arr, attr, val) {
 // ✓ Broadcast a message to connected users when someone connects or disconnects 
 // ✓ Add support for nicknames
 // Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-// Add “{user} is typing” functionality
+// ✓ Add “{user} is typing” functionality
 // Show who’s online
 // Add private messaging
 // Share your improvements!
